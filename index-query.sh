4a05bd943b4b6bc519de212d7ce393e9f4de1fca
Shubham Uniyal <shubham.uniyal@gmail.com>
2021-10-24 19:44:46 +0100

    updated shell file

--- index-query.sh
+++ index-query.sh
@@ -3,6 +3,7 @@ echo "Starting the Indexing Process for Standard And English Analyser"
 java -jar target/luceneIndex-0.0.1.jar /cran/cran.all.1400.txt /cran/cran.qry
 
 echo "Trec eval process For Standard Analyser"
+cd trec_eval-9.0.7
 
 echo "Trec eval for no similarity"
 ./trec_eval ../src/main/resources/cran/QRelsCorrectedforTRECeval ../../result-standard-no-similarity.txt
