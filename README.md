0b30020f86e8c09382899bb31b90ac08871ea21a
Shubham Uniyal <shubham.uniyal@gmail.com>
2021-10-24 19:37:18 +0100

    Updated shell script and ReadME

--- README.md
+++ README.md
@@ -6,4 +6,19 @@ mvn package
 
 ## Command to run project
 
-java -jar luceneIndex-0.0.1.jar /cran/cran.all.1400.txt /cran/cran.qry
+java -jar target/luceneIndex-0.0.1.jar /cran/cran.all.1400.txt /cran/cran.qry
+
+
+## Command to test trec eval
+./trec_eval ../src/main/resources/cran/QRelsCorrectedforTRECeval ../../result-standard-no-similarity.txt
+./trec_eval ../src/main/resources/cran/QRelsCorrectedforTRECeval ../../result-standard-vsm-similarity.txt
+./trec_eval ../src/main/resources/cran/QRelsCorrectedforTRECeval ../../result-standard-bm25-similarity.txt
+./trec_eval ../src/main/resources/cran/QRelsCorrectedforTRECeval ../../result-standard-multi-similarity.txt
+
+./trec_eval ../src/main/resources/cran/QRelsCorrectedforTRECeval ../../result-english-no-similarity.txt
+./trec_eval ../src/main/resources/cran/QRelsCorrectedforTRECeval ../../result-english-vsm-similarity.txt  
+./trec_eval ../src/main/resources/cran/QRelsCorrectedforTRECeval ../../result-english-bm25-similarity.txt 
+./trec_eval ../src/main/resources/cran/QRelsCorrectedforTRECeval ../../result-english-multi-similarity.txt 
+
+## Command to execute process end to end
+sh ./index-query.sh
--- index-query.sh
+++ index-query.sh
@@ -1,3 +1,25 @@
 mvn package
 echo "Starting the Indexing Process for Standard And English Analyser"
-java -jar luceneIndex-0.0.1.jar /cran/cran.all.1400.txt /cran/cran.qry
+java -jar target/luceneIndex-0.0.1.jar /cran/cran.all.1400.txt /cran/cran.qry
+
+echo "Trec eval process For Standard Analyser"
+
+echo "Trec eval for no similarity"
+./trec_eval ../src/main/resources/cran/QRelsCorrectedforTRECeval ../../result-standard-no-similarity.txt
+echo "Trec eval for vsm similarity"
+./trec_eval ../src/main/resources/cran/QRelsCorrectedforTRECeval ../../result-standard-vsm-similarity.txt
+echo "Trec eval for BM25 Similarity"
+./trec_eval ../src/main/resources/cran/QRelsCorrectedforTRECeval ../../result-standard-bm25-similarity.txt
+echo "Trec eval for Mutli Similarity using VSM and BM25"
+./trec_eval ../src/main/resources/cran/QRelsCorrectedforTRECeval ../../result-standard-multi-similarity.txt
+
+echo "Trec eval process For English Analyser"
+
+echo "Trec eval for no similarity"
+./trec_eval ../src/main/resources/cran/QRelsCorrectedforTRECeval ../../result-english-no-similarity.txt
+echo "Trec eval for vsm similarity"
+./trec_eval ../src/main/resources/cran/QRelsCorrectedforTRECeval ../../result-english-vsm-similarity.txt  
+echo "Trec eval for BM25 Similarity"
+./trec_eval ../src/main/resources/cran/QRelsCorrectedforTRECeval ../../result-english-bm25-similarity.txt 
+echo "Trec eval for Mutli Similarity using VSM and BM25"
+./trec_eval ../src/main/resources/cran/QRelsCorrectedforTRECeval ../../result-english-multi-similarity.txt    
